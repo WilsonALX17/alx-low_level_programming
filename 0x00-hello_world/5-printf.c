c9a4ed9a724d8e8bf5fba94636f80386ae090e21
WilsonALX17 <kumah95.aw34@gmail.com>
2023-06-17 16:03:53 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,24 @@
-#include <studio>
+#include<stdio.h>
 /**
- * main - A program that print a line using the printf function
- * Return: 0 (Return 0)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece
-of art,\n")
-return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
